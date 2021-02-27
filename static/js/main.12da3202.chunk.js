ba9a834251708b335ef100cefb72d7dd7156f74c
blast-wind <windsand.b@gmail.com>
2021-02-27 22:55:42 +0800

    build.2021.02.27

---
+++ static/js/main.12da3202.chunk.js
@@ -0,0 +1,3 @@
+/*! License information can be found in static/js/main.12da3202.chunk.js.LICENSE */
+(this.webpackJsonpview=this.webpackJsonpview||[]).push([[0],{10:function(e,c,t){"use strict";t.r(c);var n=t(1),c=t.n(n),n=t(3),n=t.n(n),s=(t(8),t(0));n.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("header",{className:"App-header",children:Object(s.jsx)("h1",{children:99})})})},{})}),document.getElementById("root"))},8:function(e,c,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.12da3202.chunk.js.map
